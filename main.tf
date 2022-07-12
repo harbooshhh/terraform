9f2d15685888de0dfc816116ab684be3aa23d9b8
harbooshhh <harbooshhh@gmail.com>
2022-07-12 20:35:34 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
   ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "HelloWorld...v2"
   }
 }
